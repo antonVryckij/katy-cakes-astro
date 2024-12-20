28198789ef4cb8b66bdbbe476cb3886307fbd768
Anton Vrutskyi <a.vrutskyi@steel.mil.ua>
2024-12-20 14:38:46 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import pluginJs from '@eslint/js';
+import astroEslintParser from 'astro-eslint-parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroEslintParser,
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  },
+  {
+    files: ['**/*.{js,jsx,astro}'],
+    rules: {
+      'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+    },
+  },
+  eslintConfigPrettier,
+  {
+    ignores: ['dist', 'node_modules', '.github', 'types.generated.d.ts', '.astro'],
+  },
+];
