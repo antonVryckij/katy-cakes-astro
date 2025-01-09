ac6b39b91196ac231302e6cba61b297e46a2ea5f
Anton Vrutskyi <a.vrutskyi@steel.mil.ua>
2025-01-09 08:55:16 +0200

    Add and apply Rubik Variant font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,15 @@
 import prelinePlugin from 'preline/plugin';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,jsx,tsx}', 'node_modules/preline/dist/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Rubik Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [prelinePlugin],
 };
