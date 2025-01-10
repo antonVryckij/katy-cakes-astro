d64effc12626d51c694c419e4d4f29562f866a1e
Anton Vryckij <50009641+antonVryckij@users.noreply.github.com>
2025-01-10 16:28:41 +0200

    Add theme toggle button (#3)
    Co-authored-by: Anton Vrutskyi <a.vrutskyi@steel.mil.ua>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,8 @@
-import prelinePlugin from 'preline/plugin';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,jsx,tsx}', 'node_modules/preline/dist/*.js'],
+  content: ['./src/**/*.{astro,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
@@ -11,5 +10,5 @@ export default {
       },
     },
   },
-  plugins: [prelinePlugin],
+  darkMode: 'selector',
 };
