8b0cd54c4fd5671e8be670552579686fc3049496
Anton Vrutskyi <a.vrutskyi@steel.mil.ua>
2025-01-13 14:15:02 +0200

    Add responsive styes for hero section

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,10 +1,22 @@
 import type { HeaderMenuItem } from '../types/navigation.ts';
 
+export const PATH = {
+  ROOT: '/',
+  MENU: '/menu',
+  ABOUT_US: '/about-us',
+  GALLERY: '/gallery',
+  REVIEWS: '/reviews',
+  BLOG: '/blog',
+  FAQ: '/faq',
+  CONTACTS: '/contacts',
+};
+
 export const HEADER_MENU: HeaderMenuItem[] = [
-  { title: 'Про нас', src: '/about-us' },
-  { title: 'Галерея', src: '/gallery' },
-  { title: 'Відгуки', src: '/reviews' },
-  { title: 'Блог', src: '/blog' },
-  { title: 'Часті питання', src: '/faq' },
-  { title: 'Контакти', src: '/contacts' },
+  { title: 'Меню', src: PATH.MENU },
+  { title: 'Про нас', src: PATH.ABOUT_US },
+  { title: 'Галерея', src: PATH.GALLERY },
+  { title: 'Відгуки', src: PATH.REVIEWS },
+  { title: 'Блог', src: PATH.BLOG },
+  { title: 'Часті питання', src: PATH.FAQ },
+  { title: 'Контакти', src: PATH.CONTACTS },
 ];
